00d720efbbd8c84041f5b7b29db3d598fc33ea68
Meera <mpatel@concertai.com>
2021-03-30 15:54:35 -0400

    fix path expansion

--- inst/py/read_owl.py
+++ inst/py/read_owl.py
@@ -1,6 +1,7 @@
 import xmltodict
+import os
 def read_owl(owl_path):
-	owl_path = owl_path.replace('~', '..')
+	owl_path = os.path.expanduser(owl_path)
 	with open(owl_path, 'rb') as f:
 		output = xmltodict.parse(f)
 		return(output)
