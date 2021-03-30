6de7c7a033a676141873a4d17aa8b4d7b8783b2b
Meera <mpatel@concertai.com>
2021-03-30 12:59:05 -0400

    add read_owl documentation

---
+++ R/read_owl.R
@@ -0,0 +1,15 @@
+#' @title
+#' Read an OWL File
+#'
+#' @description
+#' Runs the `xmltodict.parse` in python on an OWL file.
+#'
+#' @param owl_path Path to OWL file.
+#' @rdname read_owl
+#' @export
+
+
+read_owl <-
+  function(owl_path) {
+    "dummy"
+  }
