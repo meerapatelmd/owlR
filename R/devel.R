58ee8a1b240fb1213643e411989acf8063e804e1
Meera <mpatel@concertai.com>
2021-03-30 17:17:19 -0400

    add python functions

--- R/devel.R
+++ R/devel.R
@@ -1,6 +1,24 @@
 # library(owlr)
+# library(tidyverse)
 # ncit <-
 #   read_owl(
 #     "~/OneDrive - ConcertAI/NCIt OWL/Thesaurus.owl"
 #   )
-
+#
+# ncit_sections <- names(ncit$`rdf:RDF`)
+# ncit_sections <- grep("^[@]{1}",
+#                       x = ncit_sections,
+#                       value = TRUE,
+#                       invert = TRUE)
+#
+# i <- 0
+# for (ncit_section in ncit_sections) {
+#   objName <- str_replace_all(ncit_section,
+#                              "(^.*?):(.*$)",
+#                              "\\2")
+#   i <- i+1
+#   secretary::typewrite_progress(iteration = i,
+#                                 total = length(ncit_sections))
+#   assign(x = objName,
+#          value = reticulate::py_to_r(ncit$`rdf:RDF`[ncit_section]))
+# }
--- inst/py/read_owl.py
+++ inst/py/read_owl.py
@@ -1,7 +1,21 @@
 import xmltodict
 import os
+
+def get_keys(od): 
+  output = list(od.keys())
+  return(output)
+
+def get_values(od):
+  output = list(od.values())
+  return(output)
+
 def read_owl(owl_path):
 	owl_path = os.path.expanduser(owl_path)
 	with open(owl_path, 'rb') as f:
 		output = xmltodict.parse(f)
 		return(output)
+
+def parse_owl(od): 
+	k  = get_keys(od)
+	v  = get_values(od)
+	return k, v
